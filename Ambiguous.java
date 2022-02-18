16053f100d67fe1c34139874c742597ef13f9c13
danieli97 <78556966+danieli97@users.noreply.github.com>
2022-02-18 15:27:04 -0500

    Update Ambiguous.java

--- Ambiguous.java
+++ Ambiguous.java
@@ -1,30 +1,7 @@
 package sokoban;
 
-public abstract class Ambiguous {
-	// Attributes
-	protected Location loc;
-	protected String iconFile;
-	protected boolean movable;
+public interface Ambiguous extends Clonable {
 	
-	// Methods
-	// Getters
-	public Location location() {
-		return this.loc;
-	}
-	
-	public String iconFile() {
-		return this.iconFile;
-	}
-	
-	public boolean movable() {
-		return this.movable;
-	}
-	
-	// Setters
-	public void moveTo(Location newLoc) {
-		this.loc = newLoc;
-	}
-	
-	// method that defines what happens when a player tries to move onto an object
-	public abstract void playerInteractWithThisObject();
+	public Modification makeCopy();
+
 }
