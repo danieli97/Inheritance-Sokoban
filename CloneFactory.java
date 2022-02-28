464f22ab98850542c614c529f631325bb5c79b44
danieli97 <78556966+danieli97@users.noreply.github.com>
2022-02-28 18:05:12 -0500

    Update CloneFactory.java

--- CloneFactory.java
+++ CloneFactory.java
@@ -1,4 +1,8 @@
-
+/* 
+ * CloneFactory Class
+ * 
+ * Used to clone objects when creating the board for various levels
+ */
 public class CloneFactory {
 	
 	public Modification getClone(Modification modificationSample) {
