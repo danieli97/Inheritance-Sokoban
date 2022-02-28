3a57593f623edd3539d9181f769f7d8d503b6886
danieli97 <78556966+danieli97@users.noreply.github.com>
2022-02-28 18:03:10 -0500

    Update Observer.java

--- Observer.java
+++ Observer.java
@@ -2,7 +2,7 @@
  * Observer Interface
  * 
  * Used as Observer for Observer Design Pattern
- * To be implemented by any child of the Modification class
+ * To be implemented by all children of the Modification class
  */
 public interface Observer {
 	
