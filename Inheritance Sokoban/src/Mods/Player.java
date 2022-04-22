9efd01584286883f4764aa9b89fdc1459d106656
danieli97 <78556966+danieli97@users.noreply.github.com>
2022-04-22 17:43:01 -0400

    Update Player.java

--- Inheritance Sokoban/src/Mods/Player.java
+++ Inheritance Sokoban/src/Mods/Player.java
@@ -36,7 +36,15 @@ public class Player extends Modification {
 
 		return Object;
 	}
-
+	
+	/**
+	* Returns a string variable, which can be set to any of the follwoing values: "PLEFT", "PRIGHT", "PUP", "PDOWN".
+	* The cmd argument must be one of the following: "UP", "DOWN", "LEFT", "RIGHT".
+	* Based on the cmd given, this method returns a value that specifies which direction the player has moved.
+	* This return value is then returned and given as a cmd to the other observer objects, which will be notified that the player has moved.
+	* @param 	cmd	a string that represents the direction the player has moved
+	* @return		a boolean value that represents whether or not the modification is able to move in the specified direction
+	*/
 	@Override
 	public String update(String cmd) {
 		switch (cmd) {
